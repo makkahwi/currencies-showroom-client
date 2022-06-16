e7c8c8476170a86699238dd10aea17afb784fe48
makkahwi <SuhaibAhmadAi@hotmail.com>
2022-06-16 16:29:02 +0300

    Fixed Data

---
+++ src/currenciesData.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    dissolved: false,
+    country: "Malaysia",
+    unit: "Ringgit",
+    fraction: "Cen",
+    code: "MYR",
+    exchangeRateUSD: 4.2,
+    units: [0.01, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100]
+  },
+  {
+    id: 2,
+    dissolved: false,
+    country: "Jordan",
+    unit: "Dinar",
+    fraction: "Qersh",
+    code: "JOD",
+    exchangeRateUSD: 0.7,
+    units: [0.01, 0.05, 0.1, 0.25, 0.5, 1, 5, 10, 20, 50]
+  },
+  {
+    id: 3,
+    dissolved: false,
+    country: "EU",
+    unit: "Euro",
+    fraction: "Cent",
+    code: "EUR",
+    exchangeRateUSD: 0.6,
+    units: [0.01, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500]
+  },
+];
---
+++ src/metaData.js
@@ -0,0 +1,9 @@
+export default {
+  id: 1,
+  user: "makkahwi",
+  name: "Suhaib Ahmad",
+  email: "SuhaibAhmadAi@hotmail.com",
+  location: "JOD",
+  targetLimit: 18,
+  source: "xe",
+};
