8dfa36419d4d21fe9574bf7ccbeb70789d6922e8
makkahwi <SuhaibAhmadAi@hotmail.com>
2022-07-17 10:17:22 +0300

    Fix Initial Global Axios Configs

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,4 @@
 import axios from 'axios';
-import app from '../../main';
-import store from '../../store';
 
 const baseURL = "https://currencies-showroom-suhaib.herokuapp.com/"
 
@@ -11,45 +9,32 @@ const service = axios.create({
 service.defaults.headers.get.Accept = 'application/json';
 service.defaults.headers.put['Content-Type'] = 'application/json';
 
-service.interceptors.request.use((config) => {
+service.interceptors.request.use(config => {
   const jwt = localStorage.getItem('jwt');
 
   if (jwt != null) {
     config.headers["Authorization"] = `Bearer ${jwt}`;
-  }
+  };
+
   return config;
-}, (err) => Promise.reject(err));
+}, e => Promise.reject(e));
 
 let isAlreadyFetchingAccessToken = false;
 
-service.interceptors.response.use((response) => response, (error) => {
-  console.log('ERROR', error);
+service.interceptors.response.use(response => response, e => {
+  console.log('Global Axios Error', e);
 
-  if (!error.response) return Promise.reject(error);
-  const { config, response: { status } } = error;
+  if (!e.response) return Promise.reject(e);
+  const { config, response: { status } } = e;
   const originalRequest = config;
 
   if (status === 401 && localStorage.getItem('refreshToken')) {
     if (!isAlreadyFetchingAccessToken) {
       isAlreadyFetchingAccessToken = true;
+    };
+  };
 
-      store.dispatch('api/token/refresh').then((res) => {
-        isAlreadyFetchingAccessToken = false;
-        return originalRequest;
-      });
-    }
-  }
-  return Promise.reject(error);
-});
-
-service.interceptors.request.use((config) => {
-  app.$Progress.start();
-  return config;
-});
-
-service.interceptors.response.use((response) => {
-  app.$Progress.finish();
-  return response;
+  return Promise.reject(e);
 });
 
 export default service;
